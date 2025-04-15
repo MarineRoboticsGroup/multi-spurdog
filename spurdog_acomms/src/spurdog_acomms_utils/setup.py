c1e5ffc03c9037173dc909acabcec48354fc1114
JP Morrison <morrisjp@mit.edu>
2025-04-15 17:33:07 -0400

    cleaned up launch scripting

---
+++ spurdog_acomms/src/spurdog_acomms_utils/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='spurdog_acomms',
+    version='0.1.0',
+    packages=['spurdog_acomms_utils'],
+    install_requires=[
+        # Add any dependencies here, if needed
+    ],
+)
