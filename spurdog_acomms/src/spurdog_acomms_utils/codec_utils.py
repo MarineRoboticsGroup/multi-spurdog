4e2149ea2d8e6de9d161891a5a16958a7458d9b3
JP Morrison <morrisjp@mit.edu>
2025-07-12 04:24:06 +0000

    fixed import bug

--- spurdog_acomms/src/spurdog_acomms_utils/codec_utils.py
+++ spurdog_acomms/src/spurdog_acomms_utils/codec_utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseWithCovariance
-from spurdog_acomms.msg import GraphUpdate, BetweenFactor, RangeFactor
+from spurdog_acomms.msg import BetweenFactor, RangeFactor
 from ros_acomms_msgs.msg import PingReply
 # fill out codec scale factors
 CODEC_SCALE_FACTORS = {
